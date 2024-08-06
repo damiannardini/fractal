fd98b3fb950dbbbff4b7bb31a085fb88e0ea3fc9
damiannardini <damiannardini@multiplica.com>
2024-08-06 14:39:12 +0200

    add multiple port servers instances

--- fractal.config.js
+++ fractal.config.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const path = require("path");
+const portfinder = require('portfinder');
 const fractal = (module.exports = require("@frctl/fractal").create());
-const mandelbrot = require("@frctl/mandelbrot");
 
 fractal.set("project.title", "Fractal testing");
 
@@ -12,20 +12,31 @@ fractal.components.set("ext", ".hbs");
 
 fractal.docs.set("path", path.join(__dirname, "docs"));
 
-fractal.web.set('static.path', __dirname + '/public');
+fractal.web.set("static.path", __dirname + "/public");
 
 fractal.web.set("builder.dest", __dirname + "/build");
 
-fractal.cli.command('copy-assets', (args, done) => {
-    const fs = require('fs-extra');
-    const sourcePath = path.join(__dirname, 'src', 'assets');
-    const destPath = path.join(__dirname, 'public', 'assets');
-    
-    fs.copy(sourcePath, destPath, (err) => {
-        if (err) return console.error(err);
-        console.log('Assets copiados con éxito!');
-        done();
-    });
+fractal.cli.command("copy-assets", (args, done) => {
+  const fs = require("fs-extra");
+  const sourcePath = path.join(__dirname, "src", "assets");
+  const destPath = path.join(__dirname, "public", "assets");
+
+  fs.copy(sourcePath, destPath, (err) => {
+    if (err) return console.error(err);
+    console.log("Assets copiados con éxito!");
+    done();
+  });
 });
 
+const defaultPort = 3001; // Cambia este valor al puerto que prefieras
 
+portfinder.basePort = defaultPort;
+portfinder
+  .getPortPromise()
+  .then((port) => {
+    fractal.web.set("server.port", port);
+    console.log(`Fractal will run on port ${port}`);
+  })
+  .catch((err) => {
+    console.error("No free port found", err);
+  });
---
+++ src/patterns/molecules/button/README.md
@@ -0,0 +1,3 @@
+## Botón de prueba
+
+Este es un botón de prueba que se puede utilizar para verificar si Fractal está funcionando correctamente. Si ves este botón en tu página, significa que Fractal se ha configurado correctamente.
